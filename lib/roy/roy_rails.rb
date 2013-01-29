9953b55e94b93d2279658faaee1b54e8d02bc2cf
eric thul <thul.eric@gmail.com>
2013-01-29 16:46:14 -0500

    Ensuring ::Rails::Engine is referenced

--- lib/roy/roy_rails.rb
+++ lib/roy/roy_rails.rb
@@ -1,5 +1,5 @@
 module Roy
-  class RoyRails < Rails::Engine
+  class RoyRails < ::Rails::Engine
     initializer :register_roy do |app|
       app.assets.register_engine '.roy', RoyTemplate
     end
